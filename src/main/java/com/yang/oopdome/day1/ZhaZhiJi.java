6c342d34471112a9a3549851c396acea483e6272
nowarashinnosuke <949163403@qq.com>
2019-03-24 15:49:28 +0800

    第三次提交

--- src/main/java/com/yang/oopdome/day1/ZhaZhiJi.java
+++ src/main/java/com/yang/oopdome/day1/ZhaZhiJi.java
@@ -2,8 +2,11 @@ package com.yang.oopdome.day1;
 //创建一个榨汁机的类
 public class ZhaZhiJi {
     //定义一个方法：榨汁
-    public String zhazhi(String friut){
-        String zhi=friut+"汁";
+    //方法定义处，参数叫做形式参数，也叫形参（主要约束参数的类型的）
+    //形参名字可以随便取，但是要符合驼峰命名规则，同时要有意义。
+    public String zhazhi(int num,String friut){
+        String zhi=num+"杯"+friut+"汁";
+
         return zhi;
     }
 }
--- src/test/java/com/yang/oopdome/day1/TestZhaZhiJi.java
+++ src/test/java/com/yang/oopdome/day1/TestZhaZhiJi.java
@@ -11,7 +11,16 @@ public class TestZhaZhiJi {
             System.out.println("请放入水果");
             ZhaZhiJi ji1 = new ZhaZhiJi();//调用榨汁的方法
             String shuiguo = input.next();
-            String zhi = ji1.zhazhi(shuiguo);//返回的值用相同类型的数据类型接受
+            //返回的值用相同类型的数据类型接受
+            //方法调用处，参数叫做实际参数，也叫实参
+            //实参敏子也可以随便取，但是要遵循驼峰命名规则，同时也要有意义
+            //实参和形参数据类型必须一致，名字可以一致，也可以不一致
+            //方法有没有返回值和方法有没有参数，没有任何关系
+            //方法参数可以有一个也可以有多个，多个参数中间用英文的逗号隔开（形参和实参都是这样）
+            //形参和实参的数量一定要一直，数据类型要一致，顺序要也要一直
+            System.out.println("您要几杯？");
+            int num=input.nextInt();
+            String zhi = ji1.zhazhi(num,shuiguo);
             System.out.println(zhi);
             System.out.println("是否继续榨汁（y/n）？");
             answer=input.next();
